e46b0fbd679e4b81361bfcb96f62fa9ca97d7d93
Shivang Kushwaha <91315787+shivangkushwaha@users.noreply.github.com>
2024-05-29 17:24:59 +0000

    Add migartion of WalletAddress

---
+++ migrations/20240529171138-create-wallet-addresses.js
@@ -0,0 +1,42 @@
+// migrations/[timestamp]-create-wallet-addresses.js
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('WalletAddresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('WalletAddresses');
+  }
+};
